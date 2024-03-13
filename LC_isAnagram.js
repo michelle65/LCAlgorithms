55436cd9151c81e7ca135ee4c4accaa58a1ac016
Michelle <rotariumihaela.bv@gmail.com>
2024-03-13 23:54:24 +0200

    isAngram code pushed

---
+++ LC_isAnagram.js
@@ -0,0 +1,24 @@
+function isAnagram(s,t){
+    if(s.length!==t.length){
+        return false;
+    }
+
+    const sCharCounts = {}; //{H: 1,E:1,L:2,0:1}
+
+    //Fill sCharCounts
+    for(let i=0;i<s.length;i++){
+        const sChar = s[i];
+        sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1;
+    }
+
+    for(let i=0;i<t.length;i++){
+        const tChar = t[i];
+
+        if(!sCharCounts[tChar]){
+            return false;
+        }else{
+            sCharCounts[tChar]--;
+        }
+    }
+    return true;
+}
