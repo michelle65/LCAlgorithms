8d1f199ab91eb3f57e40f444af86dbca84d19bbb
Michelle <rotariumihaela.bv@gmail.com>
2024-03-13 23:59:18 +0200

    is paranthesis valid check code

---
+++ LC_isValid.js
@@ -0,0 +1,16 @@
+const isValid= s =>{
+    let stack = [];
+    let pairsHashMap = {"(":")","{":"}","[":"]"};
+
+    for(let i =0 ;i<s.length;i++){
+        let char = s[i];
+
+        if(pairsHashMap[char]){
+            stack.pusch(char);
+        }else if(pairsHashMap[stack.pop()]!==char){
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+}
